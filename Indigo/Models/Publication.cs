1b355d5084a258d96bfed6bbedbbb976fa51c664
Martin Marinov <m.marinov1733@gmail.com>
2025-06-26 12:35:13 +0300

    корекции

--- Indigo/Models/Publication.cs
+++ Indigo/Models/Publication.cs
@@ -7,19 +7,25 @@ namespace Indigo.Models
     public class Publication
     {
         public int Id { get; set; }
+
         [Required]
         public string Title { get; set; }
+
         [Required]
         public string Topic { get; set; }
+
         [Required]
         public string Description { get; set; }
+
         [Required]
         public string AuthorName { get; set; }
+
         public DateTime CreationDate { get; set; } = DateTime.UtcNow;
         public bool IsApproved { get; set; } = false;
         public byte[]? PdfFileData { get; set; }
         public string? PdfMimeType { get; set; }
         public string? PdfFileName { get; set; }
+
         [ForeignKey(nameof(PartId))]
         public int PartId { get; set; }
         public Part Part { get; set; }
